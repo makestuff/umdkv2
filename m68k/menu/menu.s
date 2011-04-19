f7718e32c59a7a8203aa71d8c0592c98070dbf5b
Chris McClelland <github.com@m3.ath.cx>
2011-04-19 17:21:40 +0100

    Now write count to register 0 (YIELDBUS)

--- m68k/menu/menu.s
+++ m68k/menu/menu.s
@@ -12,10 +12,12 @@ vtimer		= 0x000000
 ioBase		= 0xA13000
 mdReg00		= 0
 mdReg01		= 2
+mdReg02		= 4
 
 main:	lea	ioBase, a0		| base of I/O
 	moveq	#0, d0			| init counter
-loop:	move.w	d0, mdReg01(a0)		| write counter to register 1
+loop:
+	move.w	d0, mdReg00(a0)		| write counter to register 1
 	addq.w	#1, d0			| inc counter
 	move.l	#65535, d1		| init delay count
 delay:	nop
